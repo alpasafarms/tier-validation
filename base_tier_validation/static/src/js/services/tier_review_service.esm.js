6d458bb73e9b31606297653c1eed708d2652dae2
alpasafarms <fhernandez@alpasafarms.com>
2026-03-04 20:54:39 -0600

    innit

---
+++ base_tier_validation/static/src/js/services/tier_review_service.esm.js
@@ -0,0 +1,42 @@
+import {reactive} from "@odoo/owl";
+import {registry} from "@web/core/registry";
+
+export class TierReviewService {
+    constructor(env, services) {
+        this.env = env;
+        this.store = services["mail.store"];
+        this.busService = services.bus_service;
+    }
+    setup() {
+        if (!this.store || !this.busService) {
+            return;
+        }
+        if (typeof this.store.tierReviewCounter !== "number") {
+            this.store.tierReviewCounter = 0;
+        }
+        if (!Array.isArray(this.store.tierReviewGroups)) {
+            this.store.tierReviewGroups = [];
+        }
+        this.busService.subscribe("base.tier.validation/updated", (payload) => {
+            if (payload.review_created) {
+                this.store.tierReviewCounter++;
+            }
+            if (payload.review_deleted) {
+                this.store.tierReviewCounter--;
+            }
+        });
+        this.busService.start();
+    }
+}
+
+export const tierReviewService = {
+    dependencies: ["bus_service", "mail.store"],
+
+    start(env, services) {
+        const tier_review_service = reactive(new TierReviewService(env, services));
+        tier_review_service.setup();
+        return tier_review_service;
+    },
+};
+
+registry.category("services").add("tierReviewService", tierReviewService);
